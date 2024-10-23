732b67eb125698aae15db3e70d9274f9f9eb5d32
Gaia Micciancio <gaiamicciancio@gmail.com>
2024-10-23 16:38:57 -0400

    Revert "Gaia's LogoutInputData. Looks like this will cause some issue for LogoutController which is Abeera's."
    This reverts commit f3d7fe3a37ca0d3ca895b99b53e3ad37dda53d9a.

--- src/main/java/use_case/logout/LogoutInputData.java
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -4,14 +4,9 @@ package use_case.logout;
  * The Input Data for the Logout Use Case.
  */
 public class LogoutInputData {
-    private final String username;
 
     public LogoutInputData(String username) {
-        this.username = username;
-    }
-
-    public String getUsername() {
-        return username;
+        // TODO: save the current username in an instance variable and add a getter.
     }
 
 }
